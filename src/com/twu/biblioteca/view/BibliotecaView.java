64417a783312c355a54442e5dbb98a08446fdebe
Czkl <zw407461270@163.com>
2019-04-15 12:00:54 +0800

    add `BibliotecaView` to show welcome message

---
+++ src/com/twu/biblioteca/view/BibliotecaView.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca.view;
+
+public class BibliotecaView {
+
+    public void showWelcomeMessage() {
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+}
