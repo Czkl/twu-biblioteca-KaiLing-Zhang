b2598a45f952efc8bdfe717721f4a82dfb48b610
Czkl <zw407461270@163.com>
2019-04-15 12:07:04 +0800

    add showMenu method to show menu options

--- src/com/twu/biblioteca/view/BibliotecaView.java
+++ src/com/twu/biblioteca/view/BibliotecaView.java
@@ -5,4 +5,12 @@ public class BibliotecaView {
     public void showWelcomeMessage() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
+
+    public void showMenu(){
+        System.out.println("1. List of books");
+        System.out.println("2. Checkout a book");
+        System.out.println("2. Return a book");
+        System.out.println("3. Quit");
+        System.out.println("Select（1～4）：");
+    }
 }
